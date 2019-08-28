efd1239e20ea0d0732c6f73738851a76d697dbdd
Paweł Henek <pawelhenek@gmail.com>
2019-08-28 21:57:59 +0200

    Added even more examples from tourofscala

---
+++ src/main/scala/com/github/pawelhenek/scalaplayground/tourofscala/genericClasses.scala
@@ -0,0 +1,39 @@
+package com.github.pawelhenek.scalaplayground.tourofscala
+
+object genericClasses extends App {
+
+  class Stack[A] {
+    private var elements: List[A] = Nil
+
+    def push(x: A): Unit = {
+      elements = x :: elements
+    }
+
+    def peek: A = elements.head
+
+    def pop(): A = {
+      val currentTop = peek
+      elements = elements.tail
+      currentTop
+    }
+
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    println(stack.pop)
+    println(stack.pop)
+
+    class Fruit
+    class Apple extends Fruit
+    class Banana extends Fruit
+
+    val anotherStack = new Stack[Fruit]
+    val apple = new Apple
+    val banana = new Banana
+
+    anotherStack.push(apple)
+    anotherStack.push(banana)
+
+  }
+
+}
